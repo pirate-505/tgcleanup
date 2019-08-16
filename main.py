ed903665c75ffc1f8621ed1ce0a23cf572b9dd54
pirate-505 <piratem505@gmail.com>
2019-08-16 21:46:12 +0300

    Initial commit, v1.0

---
+++ main.py
@@ -0,0 +1,175 @@
+
+import tgthingy
+import time
+import threading
+import json
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description='Manage your chats in telegram', epilog='''available actions:
+  count      -- Just count all messages in all deletable chats
+  delete     -- actually delete private chats with all messages (you will be asked anyway)
+  delete-all -- try to delete all private chats and leave all public chats and channels
+  leave-all  -- just leaves from all groups and channels, does not delete private chats''')
+parser.add_argument('action', metavar="ACTION", nargs=1, choices=["count", "delete", "delete-all", "leave-all"], help='Select one action to execute')
+parser.add_argument('--api-id', required=True, type=int, metavar="API_ID", help='Your api_id from https://my.telegram.org')
+parser.add_argument('--api-hash', required=True, metavar="API_HASH", help='Your api_hash from https://my.telegram.org')
+parser.add_argument('--test', action='store_const', const=True, help='Test confiramtion')
+#parser.add_argument('--filter-kwords-exclude', default=[], help='[WIP] Comma-separated list of words to preserve chats which contain any of these words in title')
+parser.add_argument('--lib-path', help='Provide custom path to libtdjson.so')
+parser.add_argument('--lib-localdb-dir', default='tdlib', help='Directory for local db, default: tdlib')
+parser.add_argument('--lib-localdb-key', default='my_key', help='Key for accessing local db, default: my_key')
+
+args = parser.parse_args()
+
+api_id = args.api_id
+api_hash = args.api_hash
+chat_ids = []
+chats = []
+lib_path = args.lib_path or "lib/libtdjson.so.1.4.0"  # find_library('tdjson') or 'tdjson.dll'
+localdb_dir = args.lib_localdb_dir
+localdb_key = args.lib_localdb_key
+action = args.action[0]
+#filter_kwords = args.filter_kwords_exclude
+
+
+def continue_check(text, variants_y=['y', 'yes'], variants_n=['n', 'no'], default="n", exact=False):
+    while True:
+        inp = input(text).lower() or default
+        if inp in variants_y:
+            return True
+        elif inp in variants_n:
+            return False
+        elif exact:
+            print("Please, type %s or %s" % (str(variants_y[0]), str(variants_n[0])))
+
+
+def safe_quit():
+    print("Stopping client...", end="")
+    tg.close_client()
+    print("done")
+    print("Exitting...")
+    sys.exit()
+
+
+def filter_deletable(chats):
+    return [elem for elem in chats if elem["can_be_deleted_for_all_users"]]
+
+
+def delete_act():
+    if continue_check("Delete all private chats? This cannot be undone! (yes/NO) ", variants_y=["yes"], variants_n=["no", "n"], default="no", exact=True):
+        sys.stdout.flush()
+        print("Deleting private deletable chats (this might take some time)...0    ", end="")
+        sys.stdout.flush()
+        c_count = 0
+        for c in chats_deletable:
+            #r = tg.delete_chat_history(c['id'], remove_from_chat=True, revoke=True)
+            r = {"ok": "okay"}
+            if "ok" in r:
+                c_count += 1
+                print("\b\b\b\b\b%5d" % c_count, end="")
+                sys.stdout.flush()
+        print()
+        print("Done!")
+
+
+def delete_all_act():
+    if continue_check("Delete ALL chats and groups? This cannot be undone! (yes/NO) ", variants_y=["yes"], variants_n=["no", "n"], default="no", exact=True):
+        sys.stdout.flush()
+        print("Deleting all chats and groups (this might take some time)...0    ", end="")
+        c_count = 0
+        for c in chats:
+            if c["chat_type"] == "chatTypeSupergroup" or c["chat_type"] == "chatTypeBasicGroup":
+                r = tg.leave_from_chat(c["id"])
+            else:
+                r = tg.delete_chat_history(c['id'], remove_from_chat=True, revoke=True)
+            if "ok" in r:
+                c_count += 1
+                print("\b\b\b\b\b%5d" % c_count, end="")
+                sys.stdout.flush()
+        print()
+        print("Done!")
+
+
+def count_act():
+    print("Counting all messages (this may take some time)...")
+    print("Processing chat no. 0    ", end="")
+    c_count = 0
+    total = 0
+    sys.stdout.flush()
+    for c in chats_deletable:
+        m = tg.get_full_chat_histroy(c["id"])
+        total += len(m)
+        c_count += 1
+        print("\b\b\b\b\b%5d" % c_count, end="")
+        sys.stdout.flush()
+    print()
+    print("Total messages: %d" % total)
+
+
+def leave_act():
+    if continue_check("Leave from all public chats and groups? This cannot be undone! (yes/NO) ", variants_y=["yes"], variants_n=["no", "n"], default="no", exact=True):
+        sys.stdout.flush()
+        print("Leaving all public chats and groups (this might take some time)...0    ", end="")
+        c_count = 0
+        for c in chats:
+            if c["chat_type"] == "chatTypeSupergroup" or c["chat_type"] == "chatTypeBasicGroup":
+                r = tg.leave_from_chat(c["id"])
+            r = {"ok": "okay"}
+            if "ok" in r:
+                c_count += 1
+                print("\b\b\b\b\b%5d" % c_count, end="")
+                sys.stdout.flush()
+        print()
+        print("Done!")
+
+
+actions = {"count": count_act, "delete": delete_act, "delete-all": delete_all_act, "leave-all": leave_act}
+
+print("Initializing tg client...", end="")
+
+try:
+    tg = tgthingy.TGthingy(api_id,
+                           api_hash,
+                           library_path=lib_path,
+                           verbosity=1,
+                           localdb_dir=localdb_dir,
+                           localdb_key=localdb_key)
+    tg.build_client()
+except Exception as e:
+    print("fail (%s)" % str(e))
+else:
+    print("done")
+
+
+print("Checking authorization...", end="")
+try:
+    tg.handle_auth_routine()
+except Exception as e:
+    print("fail (%s)" % str(e))
+    safe_quit()
+if tg.auth_completed:
+    print("done")
+else:
+    print("fail")
+    safe_quit()
+
+print("Getting all chats...", end="")
+chat_ids, chats = tg.get_all_chats()
+if chat_ids and chats:
+    print("done")
+    print("Got %d chats" % len(chats))
+else:
+    print("failed")
+    print("No chats found!")
+    safe_quit()
+
+
+print("Filtering only deletable chats...", end="")
+chats_deletable = filter_deletable(chats)
+print("done")
+print("%d private chats can be deleted" % len(chats_deletable))
+
+if action in actions:
+    actions[action]()
+safe_quit()
---
+++ tgthingy.py
@@ -0,0 +1,260 @@
+from ctypes import *
+import json
+import sys
+import time
+
+
+class TGthingy:
+    def __init__(self, api_id, api_hash, library_path, verbosity=1, localdb_dir="tdlib", localdb_key="my_key"):
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.verbosity = verbosity
+        if library_path:
+            self.library_path = library_path
+            self._init_lib(self.library_path)
+            self.client = None
+        else:
+            raise ValueError("No library path provided")
+        if localdb_dir:
+            self.localdb_dir = localdb_dir
+        else:
+            raise ValueError("Provide correct database directory name!")
+        if localdb_key:
+            self.localdb_key = localdb_key
+        else:
+            raise ValueError("Provide some key for local db!")
+
+        self.auth_completed = False
+
+    def _init_lib(self, library_path):
+        self.tdjson = CDLL(library_path)
+        self._td_json_client_create = self.tdjson.td_json_client_create
+        self._td_json_client_create.restype = c_void_p
+        self._td_json_client_create.argtypes = []
+
+        self._td_json_client_receive = self.tdjson.td_json_client_receive
+        self._td_json_client_receive.restype = c_char_p
+        self._td_json_client_receive.argtypes = [c_void_p, c_double]
+
+        self._td_json_client_send = self.tdjson.td_json_client_send
+        self._td_json_client_send.restype = None
+        self._td_json_client_send.argtypes = [c_void_p, c_char_p]
+
+        self._td_json_client_execute = self.tdjson.td_json_client_execute
+        self._td_json_client_execute.restype = c_char_p
+        self._td_json_client_execute.argtypes = [c_void_p, c_char_p]
+
+        self._td_json_client_destroy = self.tdjson.td_json_client_destroy
+        self._td_json_client_destroy.restype = None
+        self._td_json_client_destroy.argtypes = [c_void_p]
+
+        self._td_set_log_verbosity_level = self.tdjson.td_set_log_verbosity_level
+        self._td_set_log_verbosity_level.restype = None
+        self._td_set_log_verbosity_level.argtypes = [c_int]
+
+        self._td_set_log_verbosity_level(self.verbosity)
+
+        fatal_error_callback_type = CFUNCTYPE(None, c_char_p)
+
+        def on_fatal_error_callback(error_message):
+            print("TDLib fatal error: ", error_message)
+
+        self._td_set_log_fatal_error_callback = self.tdjson.td_set_log_fatal_error_callback
+        self._td_set_log_fatal_error_callback.restype = None
+        self._td_set_log_fatal_error_callback.argtypes = [
+            fatal_error_callback_type]
+        c_on_fatal_error_callback = fatal_error_callback_type(
+            on_fatal_error_callback)
+        self._td_set_log_fatal_error_callback(c_on_fatal_error_callback)
+
+    def build_client(self):
+        self.client = self._td_json_client_create()
+
+    def td_execute(self, query):
+        query = json.dumps(query).encode("utf-8")
+        result = self._td_json_client_execute(None, query)
+        if result:
+            result = json.loads(result.decode("utf-8"))
+        return result
+
+    def td_send(self, query, extra=None):
+        if not self.client:
+            print("Please, call build_client() first!")
+            return
+        query = json.dumps(query).encode("utf-8")
+        self._td_json_client_send(self.client, query)
+
+    def td_receive(self):
+        if not self.client:
+            print("Please, call build_client() first!")
+            return
+        result = self._td_json_client_receive(self.client, 1.0)
+        if result:
+            result = json.loads(result.decode("utf-8"))
+        return result
+
+    def _get_stamp(self):
+        return str(time.time())
+
+    def get_answer(self, query):
+        if "@extra" not in query:
+            extra = self._get_stamp()
+            query.update((("@extra", extra),))
+        else:
+            extra = query["@extra"]
+        self.td_send(query)
+        while True:
+            event = self.td_receive()
+            if event:
+                if ("@extra" in event) and (event["@extra"] == extra):
+                    return event
+
+    def close_client(self):
+        self._td_json_client_destroy(self.client)
+
+    def _get_chats(self, offset_order, offset_id=0, limit=100):
+        ans = self.get_answer({"@type": "getChats",
+                               "offset_order": offset_order,
+                               "offset_chat_id": offset_id,
+                               "limit": limit})
+        return ans["chat_ids"]
+
+    def get_all_chats(self):
+        ids = []
+        infos = []
+        offset_order = 2**63 - 1
+        offset_id = 0
+        while True:
+            recv_ids = self._get_chats(offset_order, offset_id)
+            if len(recv_ids) < 1:
+                break
+            for cid in recv_ids:
+                if not cid in ids:
+                    ids.append(cid)
+                    infos.append(self.get_short_chat_info(cid))
+            offset_order = infos[-1]["order"]
+            offset_id = ids[-1]
+        return ids, infos
+
+    def _get_chat_info(self, chat_id):
+        ans = self.get_answer({"@type": "getChat", "chat_id": chat_id})
+        return ans
+
+    def get_short_chat_info(self, chat_id):
+        info = self._get_chat_info(chat_id)
+        res = {"id": info["id"], "chat_type": info["type"]["@type"], "title": info["title"], "can_be_deleted_for_all_users": info["can_be_deleted_for_all_users"], "order": info["order"], "last_message_id": -1}
+        if "last_message" in info:
+            res["last_message_id"] = info["last_message"]["id"]
+        return res
+
+    def delete_messages(self, chat_id, message_ids, revoke=False):
+        ans = self.get_answer({"@type": "deleteMessages", "chat_id": chat_id,
+                               "message_ids": message_ids, "revoke": revoke})
+        return ans
+
+    def delete_chat_history(self, chat_id, remove_from_chat=False, revoke=False):
+        ans = self.get_answer({"@type": "deleteChatHistory",
+                               "chat_id": chat_id,
+                               "remove_from_chat_list": remove_from_chat,
+                               "revoke": revoke})
+        return ans
+
+    def leave_from_chat(self, chat_id):
+        ans = self.get_answer({"@type": "leaveChat",
+                               "chat_id": chat_id})
+        return ans
+
+    def get_full_chat_histroy(self, chat_id):
+        message_ids = []
+        from_message_id = 0
+
+        while True:
+            recv = self._get_chat_messages(chat_id, from_message_id=from_message_id)
+            recv_msgs = recv["messages"]
+            if recv["total_count"] < 1:
+                break
+            for msg in recv_msgs:
+                if msg["id"] not in message_ids:
+                    message_ids.append(msg["id"])
+            from_message_id = message_ids[-1]
+        return message_ids
+
+    def _get_chat_messages(self, chat_id, from_message_id=0, offset=0, limit=99, only_local=False):
+        ans = self.get_answer({"@type": "getChatHistory",
+                               "chat_id": chat_id,
+                               "from_message_id": from_message_id,
+                               "offset": offset,
+                               "limit": limit,
+                               "only_local": only_local})
+        return ans
+
+    def handle_auth_routine(self):
+        # horrible, isnt it?
+        stamp = self._get_stamp()
+        self.td_send({"@type": "getAuthorizationState", "@extra": stamp})
+        while True:
+            event = self.td_receive()
+            if not event:
+                continue
+            if event["@type"] == "updateAuthorizationState":
+                auth_state = event["authorization_state"]
+
+                # if client is closed, we need to destroy it and create new client
+                if auth_state["@type"] == "authorizationStateClosed":
+                    #print("Authorization routine fail!")
+                    return False
+
+                # set tdlib parameters
+                # you MUST obtain your own api_id and api_hash at https://my.telegram.org
+                # and use them in the setTdlibParameters call
+                if auth_state["@type"] == "authorizationStateWaitTdlibParameters":
+                    self.td_send({"@type": "setTdlibParameters",
+                                  "parameters": {"database_directory": self.localdb_dir,
+                                                 "use_message_database": True,
+                                                 "use_file_database": False,
+                                                 "use_secret_chats": True,
+                                                 "api_id": self.api_id,
+                                                 "api_hash": self.api_hash,
+                                                 "system_language_code": "en",
+                                                 "device_model": "Desktop",
+                                                 "system_version": "Linux",
+                                                 "application_version": "0.1",
+                                                 "enable_storage_optimizer": True}})
+
+                # set an encryption key for database to let know tdlib how to open the database
+                if auth_state["@type"] == "authorizationStateWaitEncryptionKey":
+                    self.td_send(
+                        {"@type": "checkDatabaseEncryptionKey", "key": self.localdb_key})
+
+                # insert phone number for login
+                if auth_state["@type"] == "authorizationStateWaitPhoneNumber":
+                    sys.stdout.flush()
+                    phone_number = input("Please insert your phone number: ")
+                    self.td_send(
+                        {"@type": "setAuthenticationPhoneNumber", "phone_number": phone_number})
+
+                # wait for authorization code
+                if auth_state["@type"] == "authorizationStateWaitCode":
+                    sys.stdout.flush()
+                    code = input(
+                        "Please insert the authentication code you received: ")
+                    self.td_send(
+                        {"@type": "checkAuthenticationCode", "code": code})
+
+                # wait for password if present
+                if auth_state["@type"] == "authorizationStateWaitPassword":
+                    sys.stdout.flush()
+                    password = input("Please insert your password: ")
+                    self.td_send(
+                        {"@type": "checkAuthenticationPassword", "password": password})
+
+                if auth_state["@type"] == "authorizationStateReady":
+                    self.auth_completed = True
+                    return True
+
+            #print(event)
+            sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    print("Wrong file dude")
